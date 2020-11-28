b935392b16c49665ebe6de5693a7a3bbf838c229
diaslilian <lilian.dias25@gmail.com>
2020-11-28 17:23:05 -0300

    add candidates controller

---
+++ src/controllers/candidatesController.js
@@ -0,0 +1,34 @@
+const candidates = require("../models/candidates");
+
+const createCandidate = (req, res) => {
+  candidates.countDocuments((err, count) => {
+    if (err) {
+      return res.status(424).send({ message: err.message });
+    } else {
+      const candidate = new candidates(req.body);
+
+      candidate.id = count + 1;
+      candidate.save((err) =>
+        err
+          ? res.status(424).send({ message: err.message })
+          : res.status(201).send({
+              status: true,
+              message: "Successfully included candidates!",
+            })
+      );
+    }
+  });
+};
+
+const getAllCandidates = (req, res) => {
+  console.log(req.url);
+
+  candidates.find((err, candidate) => {
+    return res.status(200).send(candidate);
+  });
+};
+
+module.exports = {
+  createCandidate,
+  getAllCandidates,
+};
