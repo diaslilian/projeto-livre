7505eb1df720a55659ca6271431bb8df4c242129
diaslilian <lilian.dias25@gmail.com>
2020-11-28 17:23:20 -0300

    add candidates model

---
+++ src/models/candidates.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const candidatesSchema = new mongoose.Schema(
+  {
+    id: { type: Number },
+    fullName: { type: String, required: true },
+    birthDate: { type: String },
+    email: { type: String, required: true },
+    phone: { type: Number },
+    city: { type: String },
+    state: { type: String },
+    educationalLevel: { type: String },
+    language: { type: Boolean },
+    whatLanguages: [[String]],
+    jobsTitle: { type: String },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const candidatesModel = mongoose.model("Candidates", candidatesSchema);
+
+module.exports = candidatesModel;
