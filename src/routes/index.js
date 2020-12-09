b5d3280ece7511d25e3265fe68f358d2e41fb2e2
diaslilian <lilian.dias25@gmail.com>
2020-12-09 16:08:57 -0300

    add description access routes index

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ router.get("/", (req, res) => {
   res.status(200).send({
     title: "Reprograma - On7 Backend 2020 - Final Project",
     version: "1.0.0",
+    routes: ["/candidates/", "/companies/"],
   });
 });
 
