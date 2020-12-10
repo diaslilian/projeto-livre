2a48a7bcc98bbc3596c53a997e227e7f855a1912
diaslilian <lilian.dias25@gmail.com>
2020-12-10 20:42:04 -0300

    add relationship models

--- src/models/companies.js
+++ src/models/companies.js
@@ -1,16 +1,36 @@
 const mongoose = require("mongoose");
+const jobs = require("./jobs");
 
 const companiesSchema = new mongoose.Schema(
   {
-    id: { type: Number },
-    name: { type: String },
-    phone: { type: Number },
-    email: { type: String },
-    jobOpportunity: { type: String },
-    companyName: { type: String },
-    city: { type: String },
-    state: { type: String },
-    diversityInclusionCommittee: { type: Boolean },
+    id: {
+      type: Number,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    companyName: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+    },
+    state: {
+      type: String,
+    },
+    diversityInclusionCommittee: {
+      type: Boolean,
+    },
+    jobs: [jobs.jobsSchema],
   },
   {
     versionKey: false,
